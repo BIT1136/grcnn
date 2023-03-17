5d9f9029a4b134da390d6e063f59dfc42813df79
yangzhuo <destroy314@outlook.com>
2023-03-17 08:10:40 +0000

    使用中文日志,调整参数

--- scripts/processor.py
+++ scripts/processor.py
@@ -1,15 +1,17 @@
 #!/root/mambaforge/envs/grcnn/bin/python
 
-import rospy
-import ros_numpy
-from sensor_msgs.msg import Image
-from geometry_msgs.msg import Point
-
 import time
+import yaml
+
 import torch
 import numpy as np
 from scipy.stats import mode
 from skimage.transform import resize
+
+import rospy
+import ros_numpy
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Point
 
 from grcnn.srv import GetGrasp
 from utils import *
@@ -17,9 +19,7 @@ from utils import *
 
 class processor:
     def __init__(self):
-        rospy.init_node("processor")
-
-        rospy.logdebug("load model")
+        rospy.logdebug("加载模型")
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.model = torch.load(
             "/root/grasp/src/grcnn/models/jac_rgbd_epoch_48_iou_0.93",
@@ -27,11 +27,19 @@ class processor:
         )
         self.model.eval()
 
+        intrinsicPath = rospy.get_param(
+            "~intrinsicPath", "src/grcnn/config/ir_camera.yaml"
+        )
+        with open(intrinsicPath, "r", encoding="utf-8") as f:
+            config = yaml.load(f, yaml.FullLoader)
+        intrinsic = config["camera_matrix"]["data"]
+        intrinsic = np.array(intrinsic).reshape(3, 3)
         # 相机内参
-        self.cx = rospy.get_param("~cx", 311)
-        self.cy = rospy.get_param("~cy", 237)
-        self.fx = rospy.get_param("~fx", 517)
-        self.fy = rospy.get_param("~fy", 517)
+        self.cx = intrinsic[0, 2]
+        self.cy = intrinsic[1, 2]
+        self.fx = intrinsic[0, 0]
+        self.fy = intrinsic[1, 1]
+        # 深度图微调参数
         self.depthScale = rospy.get_param("~depthScale", 1)
         self.depthBias = rospy.get_param("~depthBias", 0)
         rospy.logdebug(
@@ -44,8 +52,8 @@ class processor:
             self.depthBias,
         )
 
-        width = rospy.get_param("~width", 640)
-        height = rospy.get_param("~height", 480)
+        width = config["image_width"]
+        height = config["image_height"]
         self.useCrop = rospy.get_param("~useCrop", False)
         """裁剪图像至目标尺寸，否则将图像压缩至目标尺寸"""
         size = 300
@@ -62,20 +70,21 @@ class processor:
         mode:将0值替换为深度图其他值的众数"""
         self.applyGaussian = rospy.get_param("~applyGaussian", True)
         """是否对网络输出进行高斯滤波"""
-        self.publishIntermediateData = rospy.get_param("~publishIntermediateData", True)
-        """是否发布修复的深度图和网络输出"""
-        self.publishVisualisation = rospy.get_param("~publishVisualisation", True)
+        self.pubInterData = rospy.get_param("~pubInterData", True)
+        """是否发布修复的深度图和原始网络输出"""
+        self.pubVis = rospy.get_param("~pubVis", True)
         """是否发布绘制的抓取结果"""
+        self.visType = rospy.get_param("~visType", "depth")
+        """抓取结果绘制在何种图像上,depth:深度图,rgb:rgb图,q:预测的抓取质量图"""
 
-        if self.publishIntermediateData:
+        if self.pubInterData:
             self.dpub = rospy.Publisher("fixed_depth", Image, queue_size=10)
             self.qpub = rospy.Publisher("img_q", Image, queue_size=10)
             self.apub = rospy.Publisher("img_a", Image, queue_size=10)
             self.wpub = rospy.Publisher("img_w", Image, queue_size=10)
-        if self.publishVisualisation:
+        if self.pubVis:
             self.vpub = rospy.Publisher("plotted_grabs", Image, queue_size=10)
         rospy.Service("plan_grasp", GetGrasp, self.callback)
-        rospy.spin()
 
     def crop(self, img, channelFirst=False):
         if len(img.shape) == 2:
@@ -99,7 +108,7 @@ class processor:
             raise NotImplementedError
 
     def callback(self, data):
-        rospy.loginfo("start inferencing")
+        rospy.loginfo("开始推理")
         t_start = time.perf_counter()
 
         # 获取并预处理图像
@@ -107,11 +116,11 @@ class processor:
         depth_raw: np.ndarray = ros_numpy.numpify(data.depth)  # (480, 640) uint16 单位为毫米
         if self.zeroDepthPolicy == "mean":
             meanVal = depth_raw[depth_raw != 0].mean()
-            rospy.logdebug(f"replace 0 by mean = {meanVal}")
+            rospy.logdebug(f"使用平均数 = {meanVal} 替换深度图中的0值")
             depth_raw[depth_raw == 0] = meanVal
         elif self.zeroDepthPolicy == "mode":
             modeVal = mode(depth_raw[depth_raw != 0], axis=None, keepdims=False)[0]
-            rospy.logdebug(f"replace 0 by mode = {modeVal}")
+            rospy.logdebug(f"使用众数 = {modeVal} 替换深度图中的0值")
             depth_raw[depth_raw == 0] = modeVal
         if self.useCrop:
             rgb_raw = self.crop(rgb_raw)
@@ -123,7 +132,7 @@ class processor:
             depth_raw = resize(
                 depth_raw, (self.size, self.size), preserve_range=True
             ).astype(np.uint16)
-        if self.publishIntermediateData:
+        if self.pubInterData:
             self.dpub.publish(
                 ros_numpy.msgify(Image, to_u8(depth_raw), encoding="mono8")
             )
@@ -147,7 +156,7 @@ class processor:
         -0.01~0.96
         -0.90~1.36
         -3.05~60.57"""
-        if self.publishIntermediateData:
+        if self.pubInterData:
             self.qpub.publish(ros_numpy.msgify(Image, to_u8(q_img), encoding="mono8"))
             self.apub.publish(ros_numpy.msgify(Image, to_u8(ang_img), encoding="mono8"))
             self.wpub.publish(
@@ -157,28 +166,34 @@ class processor:
 
         grasps = detect_grasps(q_img, ang_img, width_img, 5)
         t_end = time.perf_counter()
-        rospy.loginfo(f"End inferencing, time cost: {(t_end - t_start)*1000:.2f}ms")
+        rospy.loginfo(f"推理完成,耗时: {(t_end - t_start)*1000:.2f}ms")
         if len(grasps) == 0:
-            raise rospy.ServiceException("No grasp detected.")
+            raise rospy.ServiceException("未检测到抓取")
         else:
-            rospy.loginfo(
-                f"{len(grasps)} grasp(s) detected: {[str(g) for g in grasps]}"
-            )
+            rospy.loginfo(f"检测到 {len(grasps)} 个抓取候选: {[str(g) for g in grasps]}")
 
         # 绘制抓取线
-        if self.publishVisualisation:
+        if self.pubVis:
             for g in grasps:
                 line, val = g.draw(minWidth, self.size)
-                # rgb_raw[line] += (
-                #     ((255, 0, 0) - rgb_raw[line]) * np.expand_dims(val, 1)
-                # ).astype(np.uint8)
-                depth_raw[line] += (val * 200).astype(np.uint16)
-                # q_img[line] += val
-            # self.vpub.publish(ros_numpy.msgify(Image, rgb_raw, encoding="rgb8"))
-            self.vpub.publish(
-                ros_numpy.msgify(Image, to_u8(depth_raw), encoding="mono8")
-            )
-            # self.vpub.publish(ros_numpy.msgify(Image, to_u8(q_img), encoding="mono8"))
+                if self.visType == "rgb":
+                    rgb_raw[line] += (
+                        ((255, 0, 0) - rgb_raw[line]) * np.expand_dims(val, 1)
+                    ).astype(np.uint8)
+                elif self.visType == "depth":
+                    depth_raw[line] += (val * 200).astype(np.uint16)
+                elif self.visType == "q":
+                    q_img[line] += val
+            if self.visType == "rgb":
+                self.vpub.publish(ros_numpy.msgify(Image, rgb_raw, encoding="rgb8"))
+            elif self.visType == "depth":
+                self.vpub.publish(
+                    ros_numpy.msgify(Image, to_u8(depth_raw), encoding="mono8")
+                )
+            elif self.visType == "q":
+                self.vpub.publish(
+                    ros_numpy.msgify(Image, to_u8(q_img), encoding="mono8")
+                )
 
         # 计算相机坐标系下的坐标
         for g in grasps:
@@ -190,17 +205,16 @@ class processor:
             pos_y = np.multiply(y - self.cy, pos_z / self.fy)
 
             if pos_z < 200:
-                rospy.logwarn(f"Depth too small: {pos_z:.3f}, ignored")
+                rospy.logwarn(f"深度值过小: {pos_z:.3f},丢弃")
                 continue
 
-            rospy.loginfo(f"target: [{pos_x:.3f},{pos_y:.3f},{pos_z:.3f}]")
+            rospy.loginfo(f"抓取方案: [{pos_x:.3f},{pos_y:.3f},{pos_z:.3f}]")
             return (Point(pos_x, pos_y, pos_z), a)
 
-        raise rospy.ServiceException("Can't find valid grasp.")
+        raise rospy.ServiceException("无法找到有效的抓取方案")
 
 
 if __name__ == "__main__":
-    try:
-        p = processor()
-    except rospy.ServiceException:
-        pass
+    rospy.init_node("processor")
+    p = processor()
+    rospy.spin()
--- scripts/provider.py
+++ scripts/provider.py
@@ -14,34 +14,29 @@ def provider():
             rospy.logwarn(e)
             continue
         else:
-            rospy.loginfo("get rgb image")
+            rospy.loginfo("获取RGB图像")
         try:
             depth = rospy.wait_for_message("/camera/depth/image_raw", Image, 10)
         except rospy.ROSException as e:
             rospy.logwarn(e)
             continue
         else:
-            rospy.loginfo("get depth image")
+            rospy.loginfo("获取深度图像")
         # rospy.loginfo("np images:\n%s\n%s\n",rgb[0],depth[0])
         try:
             rospy.wait_for_service("plan_grasp", 1)
         except rospy.ROSException as e:
-            rospy.logwarn("Wait for service failed: %s", e)
+            rospy.logwarn("等待服务失败: %s", e)
             continue
         try:
             handle = rospy.ServiceProxy("plan_grasp", GetGrasp)
             data = handle(rgb, depth)
-            rospy.loginfo("get grasp:\n%s", data)
-            # return data
+            rospy.loginfo("得到抓取规划:\n%s", data)
         except rospy.ServiceException as e:
-            rospy.logwarn("Service call failed: %s", e)
+            rospy.logwarn("服务调用失败: %s", e)
 
 
 if __name__ == "__main__":
-    try:
-        rospy.init_node("provider", disable_signals=True)
-        provider()
-    except rospy.ROSInterruptException:
-        pass
-    except KeyboardInterrupt:
-        pass
+    rospy.init_node("provider", disable_signals=True)
+    # 调用服务不需要节点，这里是为了输出日志；disable_signals=True使得可以用ctrl+c退出
+    provider()
